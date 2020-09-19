f82d57dd279569772ebf6e63207f0145d327f581
Vrinda <vrindachoudhary11@gmail.com>
2020-09-19 22:24:37 +0530

    first Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    let menu = document.getElementById("menu").classList.toggle("action");
+  //  menu.classList.toggle("action");
+    
+  }
